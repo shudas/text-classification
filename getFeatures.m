f6f4e0258ad84f2daafa03961756c436d79220f4
shu <shu@continuuity.com>
2014-11-05 02:00:08 -0500

    multinomial logistic regression works on data\!\!

--- getFeatures.m
+++ getFeatures.m
@@ -1,4 +1,4 @@
-function [ feats ] = getFeatures( docs, labels )
+function [ feats, labels_mat ] = getFeatures( docs, labels )
 %getFeatures Creates features from the given data. 
 %   docs should be a cell array of cell arrays. The inner cell array is a
 %   document and the outer one is a list of all the documents. labels
@@ -6,20 +6,30 @@ function [ feats ] = getFeatures( docs, labels )
 
 % features is a 3d matrix each row represents the contents of a doc
 % line_contains_day_expr, line_contains_date_expr, line_contains_time_expr
-NUM_FEATS = 3;
-MAX_LINES = max(cellfun(@length, docs));
-feats = zeros(length(docs), MAX_LINES, NUM_FEATS);
+NUM_FEATS = 1;
+% MAX_LINES = max(cellfun(@length, docs));
+MAX_LINES = sum(cellfun(@length, docs));
+% feats = zeros(length(docs), MAX_LINES, NUM_FEATS);
+feats = ones(MAX_LINES, NUM_FEATS);
 
 for i=1:length(docs)
     for j = 1:length(docs{i})
+        offset = 0;
+        if (i > 1)
+            offset = sum(cellfun(@length, docs(1:i-1)));
+        end
 %         if line contains day expr, set the 1st val of innermost to 1
         
 %         if line contains date expr, set the 2nd val of innermost to 1
 
 %         if line contains time expr, set the 3rd val of innermost to 1
-        feats(i,j,3) = checkTimeExpr(docs{i}{j});
+%         feats(i,j,3) = checkTimeExpr(docs{i}{j});
+        feats(offset + j, 1) = checkTimeExpr(docs{i}{j});
     end
 end
+
+labels_mat = convertLabels(labels);
+
 end
 
 function [ weight ] = checkTimeExpr(val_to_check)
@@ -42,9 +52,24 @@ function [ weight ] = checkTimeExpr(val_to_check)
     for i=1:length(EXPS)
 %         the most strict match has highest weight
         if (regexp(val_to_check, EXPS{i}))
-            weight = (length(EXPS) - i) + 1;
+            weight = ((length(EXPS) - i) + 1) * 2;
             return;
         end
     end
-    weight = 0;
+    weight = 1;
+end
+
+function[ labels_mat ] = convertLabels(labels_cell)
+%     MAX_LINES = max(cellfun(@length, labels_cell));
+    MAX_LINES = sum(cellfun(@length, labels_cell));
+%     labels_mat = zeros(length(labels_cell), MAX_LINES);
+    labels_mat = zeros(MAX_LINES, 1);
+    for i=1:length(labels_cell)
+        offset = 0;
+        if (i > 1)
+            offset = sum(cellfun(@length, labels_cell(1:i-1)));
+        end
+        labels_mat(offset + 1:offset + length(labels_cell{i})) = labels_cell{i} + 1;
+%         labels_mat(i, 1:length(labels_cell{i})) = labels_cell{i};
+    end
 end
---
+++ runMultinomialLogistic.m
@@ -0,0 +1,25 @@
+function [ accuracy ] = runMultinomialLogistic( trainSize, testSize )
+%runMultinomialLogistic Runs multinomial logistic regression with the given
+%sizes for the training and testing data. If trainSize and testSize are not
+%provided, they default to 500 and 50 respectively.
+
+if (nargin == 0)
+    trainSize = 500;
+    testSize = 50;
+end
+
+[trainDocs, trainDocsLabels] = generateData(trainSize);
+[trainFeats, trainLabels] = getFeatures(trainDocs, trainDocsLabels);
+
+logFit = mnrfit(trainFeats, trainLabels);
+
+[testDocs, testDocsLabels] = generateData(testSize);
+[testFeats, testLabels] = getFeatures(testDocs, testDocsLabels);
+
+testPred = mnrval(logFit, testFeats);
+[maxProb, predLabel] = max(testPred, [], 2);
+numWrong = sum(predLabel - testLabels ~= 0);
+accuracy = 1 - (numWrong / length(testLabels));
+
+end
+
